12f0df9246f6c6d7ba2817177238fce0ff5a77f5
Chirag Khanna <ckhanna@horizontal.com>
2023-03-03 15:20:51 +0530

    changed layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import "@/styles/globals.css";
 import Head from "next/head";
 import type { AppProps } from "next/app";
-import Layout from "@/components/layout/layout";
+import Layout from "@/components/layout/Layout";
 import { NotificationContextProvider } from "@/store/notification-context";
 
 export default function App({ Component, pageProps }: AppProps) {
